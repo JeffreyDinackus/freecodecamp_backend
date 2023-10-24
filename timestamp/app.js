7af4df9d118f21bc4f2a4547fa9de1cc8a048d10
JeffreyDinackus <jeff.dinackus@proton.me>
2023-10-23 20:30:55 -0400

    Deploying to heroku

---
+++ timestamp/app.js
@@ -0,0 +1,25 @@
+
+
+
+const express = require('express');
+
+
+const app = express();
+
+const port = 3000;
+
+
+app.get('/time', (req, res) => {
+  res.status(200);
+  res.send({ "unix": date.getTime(), "utc": date.toUTCString() });
+});
+
+
+
+app.listen(port, (error) => {
+  if (!error)
+    console.log("Server is Successfully Running on Port: ", port);
+  else
+    console.log("Error occurred, server can't start", error);
+}
+); 
