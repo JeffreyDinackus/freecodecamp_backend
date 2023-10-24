11dd85e44d9337ee47f5af14e821d4803bee7627
JeffreyDinackus <jeff.dinackus@proton.me>
2023-10-23 20:32:09 -0400

    Deploying to heroku

--- timestamp/app.js
+++ timestamp/app.js
@@ -9,7 +9,7 @@ const app = express();
 const port = 3000;
 
 
-app.get('/time', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200);
   res.send({ "unix": date.getTime(), "utc": date.toUTCString() });
 });
